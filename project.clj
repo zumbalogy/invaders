cb51e22cb6d6d695ccadca62027cb47ab6780a1b
zumbalogy <intrapostal@gmail.com>
2017-01-31 16:02:40 +0700

    got lanterna plugged in

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject invaders "0.1.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clojure-lanterna "0.9.7"]]
+  :main invaders.core)
--- src/invaders/core.clj
+++ src/invaders/core.clj
@@ -1,6 +1,23 @@
-(ns invaders.core)
+(ns invaders.core
+  (require [lanterna.screen :as s]
+           [lanterna.terminal :as t]
+           [lanterna.constants :as c]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; (def scr (s/get-screen))
+(def term (t/get-terminal :unix)) ; :unix :text :swing :auto :cygwin
+
+(def cursor (atom [0 0]))
+
+(def machine-on (atom true))
+
+(defn -main []
+  (t/start term)
+  (t/put-string term "press escape to exit.")
+  (t/put-string term "type away ")
+  (while @machine-on
+    (let [raw-key (t/get-key-blocking term)
+          key (str raw-key)]
+      (t/put-string term key)
+      (when (= raw-key :escape)
+        (reset! machine-on false))))
+  (t/stop term))
