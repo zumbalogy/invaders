bb9e4e172c3992136c4321556dccfa586f938e53
zumbalogy <intrapostal@gmail.com>
2017-01-31 17:40:08 +0700

    can now move ship around and shoot

--- src/invaders/core.clj
+++ src/invaders/core.clj
@@ -1,23 +1,60 @@
 (ns invaders.core
-  (require [lanterna.screen :as s]
-           [lanterna.terminal :as t]
+  (require [lanterna.terminal :as t]
            [lanterna.constants :as c]))
 
-; (def scr (s/get-screen))
-(def term (t/get-terminal :unix)) ; :unix :text :swing :auto :cygwin
+(def X (atom 0))
+(def Y (atom 0))
 
-(def cursor (atom [0 0]))
+(def shoot (atom 0))
 
 (def machine-on (atom true))
 
+(defn shutdown [term]
+  (t/clear term)
+  (t/put-string term "good bye")
+  (Thread/sleep 900)
+  (reset! machine-on false))
+
+(defn draw [term]
+  (t/clear term)
+  (t/put-string term "-^-" @X @Y)
+  (when (< 0 @shoot)
+    (let [bullet "^"]
+      (case @shoot
+        3 (t/set-fg-color term :red)
+        2 (t/set-fg-color term :red)
+        nil)
+      (doseq [y (range @Y)]
+        (t/put-string term bullet (inc @X) y))
+      (swap! shoot dec)))
+  (t/set-fg-color term :white)
+  (t/move-cursor term 0 0)
+  (Thread/sleep 20))
+
 (defn -main []
-  (t/start term)
-  (t/put-string term "press escape to exit.")
-  (t/put-string term "type away ")
-  (while @machine-on
-    (let [raw-key (t/get-key-blocking term)
-          key (str raw-key)]
-      (t/put-string term key)
-      (when (= raw-key :escape)
-        (reset! machine-on false))))
-  (t/stop term))
+  (let [term (t/get-terminal :unix) ; :unix :text :swing :auto :cygwin
+        max-w (- 80 5)
+        max-h 40]
+    (t/start term)
+    (t/clear term)
+    (t/put-string term "press escape to exit.")
+    (t/put-string term "type away ")
+    (t/move-cursor term 0 0)
+    (while @machine-on
+      (let [key (t/get-key term)]
+        (case key
+          nil nil
+          ; :tab (t/clear term)
+          :left (reset! X (max 0 (dec @X)))
+          :right (reset! X (min max-w (inc @X)))
+          :up (reset! Y (max 0 (dec @Y)))
+          :down (reset! Y (min max-h (inc @Y)))
+          ; :enter (reset! Y (min max-w (inc @Y)))
+          :escape (shutdown term)
+          \space (reset! shoot 5)
+          nil)
+          ; (do
+          ;   (t/put-string term (str key))
+          ;   (reset! X (min max-w (inc @X)))))
+        (draw term)))
+    (t/stop term)))
