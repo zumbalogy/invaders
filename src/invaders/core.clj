5c23bc48ac3dabe613e1ced130b64f61b571e1c1
zumbalogy <intrapostal@gmail.com>
2017-02-01 16:37:33 +0700

    bullets with cooldown

--- src/invaders/core.clj
+++ src/invaders/core.clj
@@ -8,7 +8,8 @@
 (def VX (atom 0))
 (def VY (atom 0))
 
-(def shoot (atom 0))
+(def shots (atom []))
+(def weapon-cooldown-stamp (atom 0))
 
 (def frame (atom 0))
 
@@ -21,19 +22,7 @@
   ; (Thread/sleep 900)
   (reset! machine-on false))
 
-(defn handle-vx [vd d]
-  (cond
-    (pos? @vd)
-    (do
-      (reset! d (min 75 (inc @d)))
-      (reset! vd (max 0 (- @vd 3))))
-    (neg? @vd)
-    (do
-     (reset! d (max 0 (dec @d)))
-     (reset! vd (min 0 (+ @vd 3)))))
-  @d)
-
-(defn handle-vy [vd d]
+(defn handle-v [vd d]
   (cond
     (pos? @vd)
     (do
@@ -48,23 +37,26 @@
 (defn status []
   (format "frame:%2d xy:[%2d %2d]Vxy:[%3d %3d]time:%d" @frame @X @Y @VX @VY @old-stamp))
 
+(defn draw-ship [term]
+  (let [new-x (handle-v VX X)
+        new-y (handle-v VY Y)]
+    (t/put-string term "⋰⋱" new-x new-y)))
+
+(defn draw-shots [term]
+  (doseq [n @shots]
+    (let [bullet "^"]
+      (t/put-string term bullet (:x n) (:y n))))
+  (reset! shots
+    (filter #(< 0 (:y %))
+      (map (fn [n] {:x (:x n) :y (dec (:y n))}) @shots))))
+
 (defn draw [term]
   (t/clear term)
-  (let [new-x (handle-vx VX X)
-        new-y (handle-vy VY Y)]
-    (t/put-string term "⋰⋱" new-x new-y)
-    (when (< 0 @shoot)
-      (let [bullet "^"]
-        (case @shoot
-          3 (t/set-fg-color term :red)
-          2 (t/set-fg-color term :red)
-          nil)
-        (doseq [y (range new-y)]
-          (t/put-string term bullet (inc new-x) y))
-        (swap! shoot dec)))
-    (t/set-fg-color term :white)
-    (t/put-string term (status) 0 0)
-    (t/move-cursor term 0 0)))
+  (draw-ship term)
+  (draw-shots term)
+  (t/set-fg-color term :white)
+  (t/put-string term (status) 0 0)
+  (t/move-cursor term 0 0))
 
 (defn delta-v [d n]
   (reset! d (min 100 (max -100 (+ @d n)))))
@@ -79,6 +71,12 @@
     (when (pos? pause)
       (Thread/sleep pause))
     (reset! old-stamp (System/currentTimeMillis))))
+
+(defn fire-weapon []
+  (let [new-stamp (System/currentTimeMillis)]
+    (when (< 800 (- new-stamp @weapon-cooldown-stamp))
+      (reset! weapon-cooldown-stamp (System/currentTimeMillis))
+      (swap! shots conj {:x @X :y @Y}))))
 
 (defn -main []
   (let [term (t/get-terminal :unix) ; :unix :text :swing :auto :cygwin
@@ -102,7 +100,7 @@
           :right (delta-v VX 16)
           \d (delta-v VX 16)
           :escape (shutdown term)
-          \space (reset! shoot 5)
+          \space (fire-weapon)
           nil)
         (draw term)
         (tick-frame)))
